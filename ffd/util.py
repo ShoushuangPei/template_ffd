3095a237822bc014346319030d946dddac1214c3
qifengle <71423161+ShoushuangPei@users.noreply.github.com>
2021-04-26 10:09:31 +0800

    Update util.py

--- ffd/util.py
+++ ffd/util.py
@@ -3,6 +3,9 @@ import numpy as np
 
 def mesh3d(x, y, z, dtype=np.float32):
     grid = np.empty(x.shape + y.shape + z.shape + (3,), dtype=dtype)
+    #np.empty产生随机矩阵
+    #x.shape返回(x,y)类型
+    #np.newaxis增加一个维度
     grid[..., 0] = x[:, np.newaxis, np.newaxis]
     grid[..., 1] = y[np.newaxis, :, np.newaxis]
     grid[..., 2] = z[np.newaxis, np.newaxis, :]
